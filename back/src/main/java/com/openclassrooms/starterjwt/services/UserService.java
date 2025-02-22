180a9f2208ffa07c20109fc6ab5d2ebacd8e6cb5
MickC0 <krysnn@gmail.com>
2025-02-22 12:10:17 +0100

    Minor front refactor. Fix delete user method

--- back/src/main/java/com/openclassrooms/starterjwt/services/UserService.java
+++ back/src/main/java/com/openclassrooms/starterjwt/services/UserService.java
@@ -1,18 +1,33 @@
 package com.openclassrooms.starterjwt.services;
 
+import com.openclassrooms.starterjwt.exception.NotFoundException;
+import com.openclassrooms.starterjwt.models.Session;
 import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.repository.SessionRepository;
 import com.openclassrooms.starterjwt.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.beans.Transient;
+import java.util.List;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final SessionRepository sessionRepository;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, SessionRepository sessionRepository) {
         this.userRepository = userRepository;
+        this.sessionRepository = sessionRepository;
     }
 
+    @Transactional
     public void delete(Long id) {
+        List<Session> sessions = sessionRepository.findAll();
+        for (Session session : sessions) {
+            session.getUsers().removeIf(u -> u.getId().equals(id));
+            sessionRepository.save(session);
+        }
         this.userRepository.deleteById(id);
     }
 
